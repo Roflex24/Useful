d746f2a3147c739834048d6b68c4b6b7436c7051
Roflex24 <Roflex24@gmail.com>
2026-08-25 12:43:11 +0300

    Рефакторинг Finance

--- finance/src/main/java/my/help/finance/general/dto/HistoricalDataResponseDto.java
+++ finance/src/main/java/my/help/finance/general/dto/HistoricalDataResponseDto.java
@@ -1,20 +1,11 @@
 package my.help.finance.general.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 import java.util.List;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class HistoricalDataResponseDto {
-    private LocalDate snapshotDate;                    // За какой месяц данные
-    private List<AccountResponseDto> accounts;         // Счета на эту дату
-    private FinanceSummaryDto summary;                 // Сводка на эту дату
-    private List<BankCashbackSummaryDto> cashbackSummary; // Кешбек сводка
-}
+public record HistoricalDataResponseDto(
+        LocalDate snapshotDate,                    // За какой месяц данные
+        List<AccountResponseDto> accounts,         // Счета на эту дату
+        FinanceSummaryDto summary,                 // Сводка на эту дату
+        List<BankCashbackSummaryDto> cashbackSummary // Кешбек сводка
+) {}
