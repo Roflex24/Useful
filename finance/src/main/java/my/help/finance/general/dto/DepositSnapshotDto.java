cc5df10ec321b102fe8dd97e7fed03b50e8b176e
Roflex24 <Roflex24@gmail.com>
2026-08-25 12:38:41 +0300

    Рефакторинг Finance

--- finance/src/main/java/my/help/finance/general/dto/DepositSnapshotDto.java
+++ finance/src/main/java/my/help/finance/general/dto/DepositSnapshotDto.java
@@ -1,20 +1,11 @@
 package my.help.finance.general.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class DepositSnapshotDto {
-    private Long id;
-    private LocalDate endDate;
-    private LocalDate interestPaymentDate;
-    private BigDecimal interestRate;
-}
+public record DepositSnapshotDto(
+        Long id,
+        LocalDate endDate,
+        LocalDate interestPaymentDate,
+        BigDecimal interestRate
+) {}
