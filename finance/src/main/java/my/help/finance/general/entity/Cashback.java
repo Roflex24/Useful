19676a96372f47594039afef22b7dd4f9ae4d1c1
Roflex24 <Roflex24@gmail.com>
2026-08-25 14:45:37 +0300

    Рефакторинг Finance

--- finance/src/main/java/my/help/finance/general/entity/Cashback.java
+++ finance/src/main/java/my/help/finance/general/entity/Cashback.java
@@ -1,16 +1,14 @@
 package my.help.finance.general.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "cashbacks")
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- finance/src/main/java/my/help/finance/general/entity/Deposit.java
+++ finance/src/main/java/my/help/finance/general/entity/Deposit.java
@@ -1,17 +1,15 @@
 package my.help.finance.general.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "deposits")
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
