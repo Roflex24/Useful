1e7f50093b42ff2ed4d17a4cc34804141e28cd92
Roflex24 <Roflex24@gmail.com>
2026-08-27 16:00:42 +0300

    Общий рефакторинг

--- common/src/main/java/my/help/useful/UsefulApplication.java
+++ common/src/main/java/my/help/useful/UsefulApplication.java
@@ -1,11 +1,22 @@
 package my.help.useful;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Life Management API",
+                version = "1.0.0",
+                description = "API для управления жизнью",
+                license = @License(name = "Github", url = "https://github.com/Roflex24/Useful")
+        )
+)
 @SpringBootApplication(scanBasePackages = {
         "my.help.finance",
         "my.help.useful",
